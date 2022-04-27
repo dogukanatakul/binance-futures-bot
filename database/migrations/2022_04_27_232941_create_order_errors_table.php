f14467edb0ccb79ddaeb38add13ff92009d4fe9d
dogukanatakul <datakul@yandex.com>
2022-04-28 00:06:16 +0300

    error initial.

---
+++ database/migrations/2022_04_27_232941_create_order_errors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_errors', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger("orders_id")->unsigned();
+            $table->foreign("orders_id")->references("id")->on("orders");
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_errors');
+    }
+};
