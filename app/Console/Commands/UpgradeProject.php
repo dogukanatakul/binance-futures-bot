afc29e5628d1da0cf22d551207d0d7d484e324d4
dogukanatakul <datakul@yandex.com>
2022-05-08 01:27:39 +0300

    fix.

--- app/Console/Commands/UpgradeProject.php
+++ app/Console/Commands/UpgradeProject.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 
 class UpgradeProject extends Command
 {
@@ -27,6 +29,16 @@ class UpgradeProject extends Command
      */
     public function handle(): int
     {
+        DB::table('jobs')->truncate();
+        DB::table('failed_jobs')->truncate();
+        Artisan::call('queue:flush');
+        Artisan::call('queue:restart');
+        Artisan::call('queue:clear', [
+            '--force' => true,
+        ]);
+        Artisan::call('cache:clear');
+        Artisan::call('config:clear');
+        Artisan::call('route:clear');
         \App\Jobs\BotSetOrder::dispatch()->onQueue('bot');
         return Command::SUCCESS;
     }
