91f8420d6722a9f87e416055dd4f8f4c1a953062
dogukanatakul <datakul@yandex.com>
2022-05-11 21:10:05 +0300

    fix.

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -89,7 +89,7 @@ class BotController extends Controller
             $orderOperation['orders_id'] = $order->id;
             OrderOperation::create($orderOperation);
             if ($request->action == "ORDER_START_WAITING") {
-                OrderOperation::where('updated_at', '<', now()->tz('Europe/Istanbul')->subMinutes(5)->toDateTimeLocalString())->forceDelete();
+                OrderOperation::where('updated_at', '<', now()->tz('Europe/Istanbul')->subMinutes(1)->toDateTimeLocalString())->forceDelete();
             }
             if ((!empty($stopOrder = Order::where('status', 2)->where('bot', $bot)->first())) and $request->filled('action') and ($request->action == "STOP" or $request->action == "CLOSE" or $request->action == "CLOSE_TRIGGER")) {
                 $stopOrder->status = 3;
